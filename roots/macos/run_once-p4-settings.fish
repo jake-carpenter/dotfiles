0ae7bbae2959c6e7eb2df7ba404c458d693fba75
Jake Carpenter <jake@jakecarpenter.dev>
2025-07-20 22:11:50 -0600

    Update macOS defaults script to use yaml for settings

--- roots/macos/run_once-p4-settings.fish
+++ roots/macos/run_once-p4-settings.fish
@@ -1,53 +1,64 @@
 #! /usr/bin/env fish
 
-echo -e "⚙️ Adjusting dock settings"
-echo "    - Tile size: 48px"
-defaults write com.apple.dock "tilesize" -int "48"
-echo "    - Autohide: false"
-defaults write com.apple.dock "autohide" -bool "false"
-echo "    - Autohide time: 0.5sec"
-defaults write com.apple.dock "autohide-time-modifier" -float "0.5"
-echo "    - Disable recently used apps in the dock"
-defaults write com.apple.dock "show-recents" -bool "false"
-echo "    - Enable scroll to show App Exposé"
-defaults write com.apple.dock "scroll-to-open" -bool "true"
-killall Dock
+function set_default
+  set domain $argv[1]
+  set key $argv[2]
+  set type $argv[3]
+  set expected_value $argv[4]
+  set description $argv[5]
+  set changed_flag_name $argv[6]
 
-echo -e "⚙️ Adjusting Finder settings"
-echo "    - Set largest sidebar icon size"
-defaults write NSGlobalDomain "NSTableViewDefaultSizeMode" -int "3"
-echo "    - Show hidden files"
-defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
-echo "    - Show file extensions"
-defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
-echo "    - Use list view style"
-defaults write com.apple.finder "FXPreferredViewStyle" -string "Nlsv"
-echo "    - Sort folders first"
-defaults write com.apple.finder "_FXSortFoldersFirst" -bool "true"
-echo "    - Empty trash after 30 days"
-defaults write com.apple.finder "FXRemoveOldTrashItems" -bool "true"
-echo "    - Show path bar"
-defaults write com.apple.finder "ShowPathbar" -bool "true"
-echo "    - Disable file extension change warning"
-defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "true"
-killall Finder
+  set current_value (defaults read $domain $key 2>/dev/null)
+  set target_value $expected_value
 
-echo -e "⚙️ Adjusting MissionControl settings"
-echo -e "    - Displays have separate Spaces"
-# This one seems backwards, but it should be "0" to enable separate Spaces for each display
-defaults write com.apple.spaces "spans-displays" -bool "false"
-echo -e "    - Group windows by application"
-defaults write com.apple.dock "expose-group-apps" -bool "true"
-killall SystemUIServer
+  switch $type
+    case bool
+      if test "$expected_value" = true
+        set target_value 1
+      else
+        set target_value 0
+      end
+  end
 
-echo -e "⚙️ Adjusting input settings"
-echo -e "    - Enable 3-finger drag"
-defaults write com.apple.AppleMultitouchTrackpad "TrackpadThreeFingerDrag" -bool "true"
-echo -e "    - Disable character accents"
-defaults write NSGlobalDomain "ApplePressAndHoldEnabled" -bool "false"
-echo -e "    - Enable keyboard navigation"
-defaults write NSGlobalDomain AppleKeyboardUIMode -int "2"
+  if test "$current_value" != "$target_value"
+    echo "    - $description"
+    defaults write $domain $key "-$type" $expected_value
+    set -g $changed_flag_name true
+  end
+end
 
-echo -e "⚙️ Adjusting iOS simulator settings"
-echo "    - Save screenshots to desktop"
-defaults write com.apple.iphonesimulator "ScreenShotSaveLocation" -string "~/Desktop"
+set yaml_file (chezmoi source-path)/.chezmoidata/defaults.yaml
+
+set num_groups (yq '.defaults | length' $yaml_file)
+
+for i in (seq 0 (math $num_groups - 1))
+  set group_name (yq ".defaults[$i] | keys | .[0]" $yaml_file)
+  set group_data_query ".defaults[$i].$group_name"
+
+  set message (yq "$group_data_query.message" $yaml_file)
+  echo -e "⚙️ $message"
+
+  set changed_flag_name "$group_name""_changed"
+  set $changed_flag_name false
+
+  set num_entries (yq "$group_data_query.entries | length" $yaml_file)
+  for j in (seq 0 (math $num_entries - 1))
+    set entry_query "$group_data_query.entries[$j]"
+    set domain (yq "$entry_query.domain" $yaml_file)
+    set key (yq "$entry_query.key" $yaml_file)
+    set type (yq "$entry_query.type" $yaml_file)
+    set expected_value (yq "$entry_query.expected_value" $yaml_file)
+    set description (yq "$entry_query.description" $yaml_file)
+
+    set_default $domain $key $type $expected_value $description $changed_flag_name
+  end
+
+  if set -q (echo $changed_flag_name)
+    if test $$changed_flag_name = true
+      set kill_process (yq "$group_data_query.kill" $yaml_file)
+      if test "$kill_process" != "null"
+        killall $kill_process
+      end
+    end
+  end
+end
