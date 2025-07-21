6d1634c234623a1a0b6124c46947f91b94c30ddb
Jake Carpenter <jake@jakecarpenter.dev>
2025-07-21 07:48:48 -0600

    Fix settings script

--- roots/macos/run_once_p4-defaults.fish
+++ roots/macos/run_once_p4-defaults.fish
@@ -26,8 +26,8 @@ function set_default
     set -g $changed_flag_name true
   end
 end
-
-set yaml_file (chezmoi source-path)/.chezmoidata/defaults.yaml
+echo -e "path: $USER_CHEZMOI_SOURCE_DIR"
+set yaml_file "$USER_CHEZMOI_SOURCE_DIR/.chezmoidata/defaults.yaml"
 
 set num_groups (yq '.defaults | length' $yaml_file)
 
